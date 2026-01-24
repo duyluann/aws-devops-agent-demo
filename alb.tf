4cb116c76b86cdb0111658e5b59bd050d444fa44
duyluann <dl97.app@gmail.com>
2026-01-24 12:12:55 +0700

    fix(alb): reduce health check interval from 30 to 5 seconds

--- alb.tf
+++ alb.tf
@@ -32,7 +32,7 @@ resource "aws_lb_target_group" "main" {
 
   health_check {
     enabled             = true
-    interval            = 30
+    interval            = 5
     path                = "/health"
     protocol            = "HTTP"
     timeout             = 10
